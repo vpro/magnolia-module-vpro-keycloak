742ebf0823286c10b719787635a34e74cabd5394
Rico Jansen <rico.jansen@gmail.com>
2017-07-07 16:59:36 +0200

    MGNL-15210 Logout filter goed zetten.

--- src/main/java/nl/vpro/magnolia/module/keycloak/security/KeycloakLogoutFilter.java
+++ src/main/java/nl/vpro/magnolia/module/keycloak/security/KeycloakLogoutFilter.java
@@ -6,14 +6,18 @@ package nl.vpro.magnolia.module.keycloak.security;
 
 import info.magnolia.cms.security.LogoutFilter;
 import info.magnolia.init.MagnoliaConfigurationProperties;
+import lombok.extern.slf4j.Slf4j;
 
 import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 
 /**
  * @author rico
  * @date 08/06/2017
  */
+@Slf4j
 public class KeycloakLogoutFilter extends LogoutFilter {
     // used in /server/filters/logout
 
@@ -26,6 +30,12 @@ public class KeycloakLogoutFilter extends LogoutFilter {
 
     @Override
     protected String resolveLogoutRedirectLink(HttpServletRequest request) {
-        return properties.getProperty("keycloak.logout.url");
+        String encodedURI= null;
+        try {
+            encodedURI = URLEncoder.encode(String.valueOf(request.getRequestURL()), "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            log.error("Can not encode url {}", encodedURI);
+        }
+        return properties.getProperty("keycloak.logout.url")+"?redirect_uri="+ encodedURI;
     }
 }
